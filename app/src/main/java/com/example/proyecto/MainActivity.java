702291436acc54f14a15fe9fe0c2cc85ba754f44
louysotopo <40571745+louysotopo@users.noreply.github.com>
2020-11-11 22:56:02 -0500

    Vector drawable en inicio de sesión

---
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+    //public static final String EXTRA_MESSAGE = "com.example.proyecto.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/proyecto/view/RecorridoActivity.java
@@ -0,0 +1,13 @@
+package com.example.proyecto.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecorridoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
